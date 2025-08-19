311546ca4304ac8abe68b9d88e4301aa16768275
VITOR PIFFER MESSAGGI <25170637@educ123.sp.senai.br>
2025-08-18 22:12:11 -0300

    Atualização site

--- Luxor Studio/JS/encomenda.js
+++ Luxor Studio/JS/encomenda.js
@@ -1,1 +1,44 @@
-document.querySelectorAll(".cliente")
+//document.querySelector(".total").textContent=document.querySelector(".qtde").textContent * document.querySelector(".unitario").textContent
+
+//captura das encomendas
+var clientes = document.querySelectorAll(".cliente");
+
+// Calcula o valor total de todas as encomendas
+for(var count=0; count <clientes.length; count++){ //roda o for enquanto o numero de clientes for menor que a variável de contagem
+    const quantidade = clientes[count].querySelector(".qtd").textContent;
+    const unitario = clientes[count].querySelector(".unitario").textContent;
+
+    //verificação se a quantidade é valida
+    if(quantidade < 1 || isNaN(quantidade)){ //Quantidade menor que 1 ou não numerico
+        clientes[count].querySelector(".qtd ").textContent = "Quantidade errada";
+        //clientes[count].querySelector(".qtd").style.color = "red";
+        clientes[count].style.color = "red";
+    }
+
+    else{
+        clientes[count].querySelector(".total").textContent= calculoTotal(quantidade,unitario);
+    }
+
+    //verificação se o valor unitario é valido
+    if(unitario <1 || isNaN(unitario)){
+        clientes[count].querySelector(".unitario").textContent = "Valor unitario errado!";
+        clientes[count].querySelector(".unitario").style.color = "purple";
+    }
+
+    else{
+        clientes[count].querySelector(".unitario").textContent = calculoTotal(quantidade,unitario);
+    }
+}
+
+//função de Calculo total dos valores
+function calculoTotal(qtde,unit){ //variáveis não precisam ser da tabela
+    var total = 0;
+    total = qtde*unit;
+    return formatarValor(total);
+}
+
+//função formatar valor em R$
+function formatarValor(valor){
+    var valor_format = valor.toLocaleString('pt-br', {style:'currency', currency:'BRL'});
+    return valor_format;
+};
