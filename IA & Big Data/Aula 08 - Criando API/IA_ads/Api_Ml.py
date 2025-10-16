26fc6c9c9e21ce7ef2bd866e0b4c965360465cea
VITOR PIFFER MESSAGGI <25170637@educ123.sp.senai.br>
2025-10-15 21:42:09 -0300

    Criando API em IA & Big data

---
+++ IA & Big Data/Aula 08 - Criando API/IA_ads/Api_Ml.py
@@ -0,0 +1,54 @@
+import joblib 
+import pandas as pd 
+import uvicorn
+import nest_asyncio
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+#Validação de estrutura dos dados com o pydantic
+class InputData(BaseModel):
+    buying : float
+    maint : float
+    lug_boot : float
+    safety : float
+    doors_2 : int
+    doors_3 : int
+    doors_4 : int
+    doors_5more : int
+    persons_2 : int
+    persons_4 : int    
+    persons_more : int
+
+# Instanciar um objeto em fastAPI
+app = FastAPI()
+
+#Definindo a rota que recebera os dados via post
+@app.post("/api/")
+async def recebe_dados(data : InputData):
+    #Caminho para o pre processamento e do modelo
+    pre_processamento = "scaler.pkl"
+    modelo = "modelo_SVM.pkl"
+    #Carregand os modelos salvos
+    scaler = joblib.load(pre_processamento)
+    model = joblib.oad(modelo)
+    dados = {'buying' : [data.buying],
+            'maint' : [data.maint],
+            'lug_boot' : [data.lug_boot],
+            'safety' : [data.safety],
+            'doors_2' : [data.doors_2],
+            'doors_3' : [data.doors_3],
+            'doors_4' : [data.doors_4],
+            'doors_5more' : [data.doors_5more],
+            'persons_2' : [data.persons_2],
+            'persons_4' : [dados.persons_4],    
+            'persons_more' : [dados.persons_more]   
+        }
+    #Transformando o dado recebido em dataframe
+    dados_previsao = pd.DataFrame(dados)
+    #Ajustand oa escala dos dados
+    dados_previsao = scaler.transform(dado_previsao)
+    #Fazendo a previsao do m.l
+    resultado = model.predict(dados_previsao)
+    return{"resposta" : resultado[0]}
+print("Em funcionamento")
+uvicorn.run(app, host="0.0.0.0", port=8000)
