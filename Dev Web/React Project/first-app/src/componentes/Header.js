e960a1356c9bfe684ac033aedb3220382a4aaafb
Vitor Messaggi <vitormessaggi10@gmail.com>
2025-11-03 20:06:33 -0300

    Aula de react - Site responsivo

--- Dev Web/React Project/first-app/src/componentes/Header.js
+++ Dev Web/React Project/first-app/src/componentes/Header.js
@@ -1,17 +1,26 @@
 
 import '../App.css'; 
-
-
+import { useState } from 'react';
 import logo from '../assets/logo.png'; 
 
 export default function Header() {
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    const alternarMenu = () => {
+        setMenuAberto(!menuAberto);
+    }
 
     return (
         <header> {}
             {}
             <div className="divHeader"> {}
                 <img src={logo} alt="Sil Fazendo Arte" className="logo" /> {}
-                <nav> {}
+
+                
+                <button className='menu-toogle' onClick={alternarMenu}>
+                    ☰
+                </button>
+                <nav className={menuAberto ? 'menu ativo' : 'menu'}> {}
                     <ul> {}
                         <li>Home</li> {}
                         <li><a href="produtos.html">Produtos</a></li> {}
