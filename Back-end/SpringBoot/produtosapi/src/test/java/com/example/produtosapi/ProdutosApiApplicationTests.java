9d34a0d275a98a524fb94f208829b50e53363692
VITOR PIFFER MESSAGGI <25170637@educ123.sp.senai.br>
2025-11-27 18:49:25 -0300

    Spring

---
+++ Back-end/SpringBoot/produtosapi/src/test/java/com/example/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
