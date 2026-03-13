85ec20a06e4f3c3af256501c43c2a01ba2e1b90a
vitor <vitormessaggi10@gmail.com>
2026-03-13 20:56:10 -0300

    Mobile

--- Mobile/luxorautomotivo/App.tsx
+++ Mobile/luxorautomotivo/App.tsx
@@ -1,52 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { VideoView, useVideoPlayer } from "expo-video";
+//menu
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+//menu sobre
+import telaSobre from "./tela/Sobre"
+// menu botton
+const Tab = createBottomTabNavigator();
+
+
+function Menu(){
+  return <Tab.Navigator>
+          <Tab.Screen name="sobre" component={telaSobre}/>    
+        </Tab.Navigator>
+}
+//menu
 
 
 export default function App() {
-
-  const player = useVideoPlayer('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', player => {
-    player.loop = true
-    //player.play()
-  });
-
-  return (
-    <View style={styles.container}>
-      <Image source={require('./assets/logo.png')} resizeMode="contain" style={styles.logo}/>
-      <Text style={styles.text}>Luxor Stdio automotivo APP</Text>
-      <Image source={require('./assets/im3.png')} resizeMode="contain" style={styles.imagem}/>
-      <StatusBar style="light" animated />
-      <VideoView player={player}   style = {styles.video} allowsPictureInPicture />
-    </View>
-  );
+  return <NavigationContainer>
+          <Menu />
+        </NavigationContainer>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#111111",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#ffffff",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  imagem: {
-    width: 300,
-    height: 300,
-    marginTop: 20,
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
-  },
-
-  video: {
-    width: 350,
-    height: 275,
-    alignSelf: "center",
-  },
-});
