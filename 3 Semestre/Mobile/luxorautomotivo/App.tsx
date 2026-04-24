c17c52ba6654b646ac2d04818abefba939ab8a24
vitor <vitormessaggi10@gmail.com>
2026-04-24 18:54:00 -0300

    Mobile

--- 3 Semestre/Mobile/luxorautomotivo/App.tsx
+++ 3 Semestre/Mobile/luxorautomotivo/App.tsx
@@ -1,23 +1,31 @@
-//menu
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-//menu sobre
-import telaSobre from "./tela/Sobre"
-// menu botton
+// 1. IMPORTANTE: Importe o seu COMPONENTE Sobre, não a imagem!
+// Supondo que o arquivo sobre.tsx esteja na mesma pasta
+import Sobre from "./tela/Sobre"
+
 const Tab = createBottomTabNavigator();
 
-
-function Menu(){
-  return <Tab.Navigator>
-          <Tab.Screen name="sobre" component={telaSobre}/>    
-        </Tab.Navigator>
+function Menu() {
+  return (
+    <Tab.Navigator 
+      screenOptions={{
+        headerShown: false, // Esconde a barra superior se quiser um visual mais limpo
+        tabBarActiveTintColor: '#d63384', // Cor do ícone ativo
+        tabBarStyle: { backgroundColor: '#111111' } // Fundo do menu escuro
+      }}
+    >
+      {/* 2. O component agora recebe a função 'Sobre', que é a sua tela */}
+      <Tab.Screen name="Sobre" component={Sobre} />    
+    </Tab.Navigator>
+  );
 }
-//menu
-
 
 export default function App() {
-  return <NavigationContainer>
-          <Menu />
-        </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Menu />
+    </NavigationContainer>
+  );
 }
--- 3 Semestre/Mobile/luxorautomotivo/tela/Sobre.tsx
+++ 3 Semestre/Mobile/luxorautomotivo/tela/Sobre.tsx
@@ -2,21 +2,40 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { VideoView, useVideoPlayer } from "expo-video";
 
-
 export default function Sobre() {
-
-  const player = useVideoPlayer('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', player => {
-    player.loop = true
-    //player.play()
+  // Configuração do vídeo
+  const videoSource = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+  
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+    player.play(); // Descomentei para o vídeo iniciar sozinho
   });
 
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/logo.png')} resizeMode="contain" style={styles.logo}/>
-      <Text style={styles.text}>Luxor Stdio automotivo APP</Text>
-      <Image source={require('./assets/im3.png')} resizeMode="contain" style={styles.imagem}/>
+      {/* Verifique se a logo está em ../assets/ */}
+      <Image 
+      source={require('../assets/logo.png')} 
+        style={styles.logo}
+      />
+
+      <Text style={styles.text}>Luxor Studio Automotivo APP</Text>
+
+      {/* Ajustei de ./assets para ../assets assumindo que estão no mesmo lugar */}
+      <Image 
+        source={require('../assets/im3.png')}
+        resizeMode="contain" 
+        style={styles.imagem}
+      />
+
+      <VideoView 
+        player={player} 
+        style={styles.video} 
+        allowsPictureInPicture 
+        nativeControls={true} // Adicionado para você conseguir controlar o vídeo
+      />
+
       <StatusBar style="light" animated />
-      <VideoView player={player}   style = {styles.video} allowsPictureInPicture />
     </View>
   );
 }
@@ -26,27 +45,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#111111",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly", // Distrui melhor os itens na tela verticalmente
+    paddingVertical: 40,
   },
   text: {
     color: "#ffffff",
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: "bold",
+    textAlign: 'center',
   },
   imagem: {
     width: 300,
-    height: 300,
-    marginTop: 20,
+    height: 200,
   },
   logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
+    width: 150,
+    height: 150,
   },
-
   video: {
     width: 350,
-    height: 275,
-    alignSelf: "center",
+    height: 200,
+    borderRadius: 15, // Dá um toque mais moderno
+    overflow: 'hidden',
   },
 });
